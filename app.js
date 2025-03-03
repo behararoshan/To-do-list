3a19fc22707b06c6fef9a65577bd7ce4aa94faa1
BEHARA ROSHAN <behararoshan482@gmail.com>
2025-03-03 09:04:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+
+// let todo =[];
+// let req=prompt("please enter your request");
+
+// if (req=="quit"){
+//     while(true){
+//         console.log("quitting app");
+//         break;
+//     }
+//     if (req=="list"){
+//         console.laog("10---------");
+//         for (let i=0;i<todo.length;i++){
+//             console.log(i,todo[i]);
+//         }
+//         console.log("---------");
+//     }else if(req=="add"){
+//         let task = prompt("please enter the task you want to add");
+//         todo.push(task);
+//         console.log("task added");
+//     }else if(req=="delete"){
+//         let idx=prompt("please enter task index ");
+//         todo.splice(idx,1);
+//         console.log("task deleted");
+//     }else{
+//         console.log("wrong request")
+//     }
+//      req=prompt("please enter your request");
+// };
+
+
+
+// const max =prompt("enter the max number");
+
+// const random =Math.floor(Math.random()*max)+1;
+
+
+// let guess = prompt("guess the number");
+// while(true){
+//     if (guess=="quit"){
+//         console.log("user quit");
+//         break;
+//     }
+//     if (guess==random){
+//         console.log("your right");
+//         break;
+//     }else if(guess<random){
+//         guess =prompt(" hint: your guess was too small,try again")
+//     }else if(guess>random){
+//         guess =prompt(" hint: your guess was too large,try again")
+//     }
+//     else{
+//         guess=prompt("please try again");
+//     }
+// }
+// ------------------------------
+// let arr=[8,9,10,1,2,3,4,5,6,7];
+// let num=5;
+// //elementslargerthananumbernum
+// function getElements(arr,num){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]>num){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+// getElements(arr,num);
+// ----------------------
